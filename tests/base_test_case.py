b3dde7aa1bb9f23b6f1961a6cd6cc16e2e57f785
Richard Hayes <rich@justcompile.it>
2016-01-22 13:38:10 +0000

    removed print statements

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -3,12 +3,9 @@ import unittest
 
 class BaseTestCase(unittest.TestCase):
     def assertIn(self, member, container, msg=None):
-        print "OMG DO SOMETHING"
         if hasattr(unittest.TestCase, 'assertIn') and callable(getattr(unittest.TestCase, 'assertIn', None)):
-            print 'Calling super'
             super(BaseTestCase, self).assertIn(member, container, msg)
         else:
-            print 'meh'
             self.assertTrue(member in container,
                             msg="{item} not found in {iterable}"
                                  .format(item=member,
