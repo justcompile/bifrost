ee26bb69369ce90a697f4ab63c4a7e367677c720
Richard Hayes <rich@justcompile.it>
2016-01-29 10:41:42 +0000

    install packages now detects application type for help with installing packages

--- bifrost/generators/config.py
+++ bifrost/generators/config.py
@@ -8,22 +8,35 @@ from bifrost.version import get_version
 
 
 class Config(object):
+    """
+    Config class contains a number of methods for interacting with the Bifrost
+    configuration data
+    """
     @staticmethod
     def load(name='bifrost.cfg'):
-        with(open(name)) as fp:
-            return yaml.load(fp.read())
+        """
+        Loads in the configuration file for the current project. Can override
+        the name of which file to load via the `name` parameter
+        """
+        with(open(name)) as file_pointer:
+            return yaml.load(file_pointer.read())
 
     @staticmethod
     def load_from_template():
-        tmpl_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                '../_templates/config-yaml.tpl')
+        """
+        Loads the Bifrost configuration template
+        """
+        tmpl_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../_templates/config-yaml.tpl')
 
-        with(open(tmpl_path)) as fp:
-            return yaml.load(fp.read())
+        with(open(tmpl_path)) as file_pointer:
+            return yaml.load(file_pointer.read())
 
     @staticmethod
-    def save(name='bifrost.cfg', application_type='undefined', connection={},
-             deployment={}, repository=None, roles={}, **kwargs):
+    def save(name='bifrost.cfg', application_type='undefined', connection=None,
+             deployment=None, repository=None, roles=None):
+        """
+        Saves a given configuration data to a file in the current directory.
+        """
         tmpl_data = deepcopy(Config.load_from_template())
 
         bifrost_info = tmpl_data.get('bifrost', {})
@@ -31,18 +44,21 @@ class Config(object):
 
         tmpl_data['bifrost'] = bifrost_info
 
-        tmpl_data['connection'].update(connection)
-        tmpl_data['deployment'].update(deployment)
+        tmpl_data['connection'].update(connection or {})
+        tmpl_data['deployment'].update(deployment or {})
         if repository:
             tmpl_data['repository'] = repository
 
-        tmpl_data['roles'] = roles
+        tmpl_data['roles'] = roles or {}
         tmpl_data['application']['type'] = application_type
 
-        with(open(name, 'w')) as fp:
-            fp.write(yaml.dump(tmpl_data))
+        with(open(name, 'w')) as file_pointer:
+            file_pointer.write(yaml.dump(tmpl_data))
 
     @staticmethod
     def _get_file_path():
+        """
+        Retrieves the path of the configuration template
+        """
         return os.path.join(os.path.abspath(os.path.dirname(__file__)),
                             '_templates/config-yaml.tpl')
